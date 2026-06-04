6c9d0c9cd435ed38592f725d873a5a25c4164589
daamaleman <diedereicha@uamv.edu.ni>
2026-06-03 19:48:17 -0600

    feat: crear entidad Room 'Prestamo' para control de préstamos y devoluciones

---
+++ app/src/main/java/ni/edu/uam/inventarioacademicopractica/data/local/entity/Prestamo.kt
@@ -0,0 +1,15 @@
+package ni.edu.uam.inventarioacademicopractica.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "prestamos")
+data class Prestamo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val equipoId: Int,
+    val solicitante: String,
+    val fechaPrestamo: String,
+    val fechaDevolucion: String?
+)
